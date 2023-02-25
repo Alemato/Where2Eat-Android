97199567fec4140b8eaf8ffaaae9e7744e0bbd92
Stefano Decina <stefano.decina@student.univaq.it>
2023-02-25 09:23:14 +0100

    binding views

--- app/src/main/java/com/example/where2eat/HomeFragment.java
+++ app/src/main/java/com/example/where2eat/HomeFragment.java
@@ -9,11 +9,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.where2eat.databinding.FragmentHomeBinding;
+
 public class HomeFragment extends Fragment {
+
+    FragmentHomeBinding binding = null;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
