386a7f1e1496a52235e2114ec62ef2b1f692933a
Stefano Decina <stefano.decina@student.univaq.it>
2023-02-25 10:53:23 +0100

    download service e requests volley

---
+++ app/src/main/java/com/example/where2eat/service/Requests.java
@@ -0,0 +1,19 @@
+package com.example.where2eat.service;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class Requests {
+
+    private RequestQueue queue;
+
+    public Requests(Context context) {
+        queue = Volley.newRequestQueue(context);
+    }
+
+    public RequestQueue getQueue() {
+        return queue;
+    }
+}
